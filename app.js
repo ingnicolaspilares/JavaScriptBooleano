1ffa36d87132ffd2bdfb9e4f952042b5c1479d46
ingnicolaspilares <79386077+ingnicolaspilares@users.noreply.github.com>
2021-02-22 18:35:53 -0600

    First Commit

---
+++ app.js
@@ -0,0 +1,74 @@
+/* let num1 = 59;
+let apellido = null;
+num1 = 80;
+
+const name = "nico"
+
+document.write(num1); */
+
+/* let nombre = prompt("Bienvnvenidos a mi pagina ¿Cual es tu nombre?");
+
+let edad = prompt("¿Que edad tienes?, ");
+
+console.log("Hola " +nombre+ " tu edad es: " +edad);
+
+confirm("Estas deacuerdo con el mensaje del precidente"); */
+
+//Operadores de asignacion.
+
+/* let name = "Pedro";
+
+let num1 = 45;
+
+num1 %= 2;
+
+document.write(num1); */
+
+//Operadores aritmeticos
+
+/* let numero1 = 6;
+let numero2 = 3;
+
+res = -numero1;
+
+/* let res =  (-numero1 + numero2); */
+
+/* document.write(res); */
+
+//Operadores de comparacion (Booleanos true o false)
+
+/* let numero1 = 6;
+let numero2 = 6;
+
+let res = (numero1 >= numero2);
+
+document.write(res); */
+
+//Operadores logicos: Nos devuelve un resultado a partir de que cumpla o no una condicion, su resultado es booleano, y sus operandos son valores logicos o asimilables a ellos
+
+/* let valor1 = 12;
+let valor2 = 24;
+
+let res = !(false || false);
+
+document.write(res);
+
+//Si las dos condiciones se cumple se pone true o false y si no se cumple algunas de las dos me devuelve false o true
+let res =  valor1 && valor2;
+//Si cualquiera de las dos es verdades es true
+let res2 = valor1 || valor2;
+//Regresar lo contrario de lo que se devuelve
+let res3 = !valor1; */
+
+
+//Condiciones if if else else
+
+num1 = 12;
+num2 = 24;
+num3 = 25;
+num4 = 92;
+num5 = 91;
+
+op = (num1 < num2 || num2 < num3) && (!(num1 != num2) && num5 != num3);
+
+document.write(op);
